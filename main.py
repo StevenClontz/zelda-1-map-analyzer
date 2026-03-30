96e7c2852fb657fac54e29a2e633a64bc0dcab3d
Steven Clontz <steven.clontz@gmail.com>
2026-03-30 20:24:19 +0000

    add networkx requirement

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import networkx as nx
 from analyzer.extrator import DataExtractor
 
 with open("zelda1.nes", "rb") as f:
     d = DataExtractor(f)
     print(d.level_info)
+
+    # TODO: convert each level of Zelda1 into a networkx graph object:
+    # import networkx as nx
+    # G = nx.Graph()
