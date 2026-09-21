352c52381537deb65a278945b5eaf15198cea2f2
Steven Clontz <steven.clontz@gmail.com>
2026-09-21 21:22:58 +0000

    add locks

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ with open("zelda1.nes", "rb") as f:
 
     for level in range(1,10):
         G = nx.Graph()
-        color_list = []
+        node_color_list = []
+        edge_color_list = []
         for room in d.data[level].values():
             # print(room)
             coord = (room['col'],room['row'])
@@ -16,14 +17,22 @@ with open("zelda1.nes", "rb") as f:
             if room["item_info"] == "D Key" or room["item_info"] == "Key":
                 node_color = "orange"
             G.add_node(coord,pos=coord,east=room["east.wall_type"],north=room["north.wall_type"])
-            color_list += [node_color]
+            node_color_list += [node_color]
         for v in G.nodes():
             DOORS = ["Door", "Locked Door", "Shutter Door", "Bomb Hole"]
             if G.nodes[v]["east"] in DOORS:
+                color = "black"
+                if G.nodes[v]["east"] == "Locked Door":
+                    color = "red"
                 G.add_edge(v,(v[0]+1,v[1]))
+                edge_color_list += [color]
             if G.nodes[v]["north"] in DOORS:
+                color = "black"
+                if G.nodes[v]["north"] == "Locked Door":
+                    color = "red"
                 G.add_edge(v,(v[0],v[1]+1))
-        nx.draw(G,pos={v:v for v in G.nodes()}, node_color=color_list)
+                edge_color_list += [color]
+        nx.draw(G,pos={v:v for v in G.nodes()}, node_color=node_color_list, edge_color=edge_color_list)
         plt.savefig(f"level{level}.png", format="PNG")
         plt.clf()
 
